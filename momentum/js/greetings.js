2f3a960c77ee7e8e389ceef36d34c98e9a6ef68e
yunji <66745172+J-yun-ji@users.noreply.github.com>
2024-01-17 02:09:59 +0900

    Update greetings.js

--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -1,22 +1,38 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const loginafter = document.querySelector("#loginafter");
 const greeting = document.querySelector("#greeting");
+const logoutBtn = document.querySelector("#logout");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+// 로그인.
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginForm.className = "hidden";
   const userName = loginInput.value;
   localStorage.setItem(USERNAME_KEY, userName);
   paintGreetings(userName);
 }
 
-function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+// 로그아웃.
+function onLogoutSubmit(event) {
+  event.preventDefault();
+  loginafter.classList.add(HIDDEN_CLASSNAME);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginInput.value = "";
+  greeting.innerText = "";
+  localStorage.removeItem(USERNAME_KEY, "");
 }
+
+// 이름 인사 표시 및 투두 hidden 제거.
+function paintGreetings(username) {
+  loginForm.className = "hidden";
+  loginafter.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Good Morning ! ${username}`;
+}
+
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername == null) {
@@ -27,3 +43,5 @@ if (savedUsername == null) {
   // show the greetins
   paintGreetings(savedUsername);
 }
+
+logoutBtn.addEventListener("click", onLogoutSubmit);
