3bda395ad65aa54a2cc2123935aad6c466e0f7f8
yunji <66745172+J-yun-ji@users.noreply.github.com>
2024-01-17 02:22:06 +0900

    Update background.js

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -4,7 +4,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `../img/${chosenImage}`;
+bgImage.src = `/momentum/img/${chosenImage}`;
 bgImage.alt = "background image";
 
 document.body.appendChild(bgImage);
