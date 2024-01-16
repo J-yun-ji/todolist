1a15128afa4b4e76c04f82a314163ede3dbfdd19
yunji <66745172+J-yun-ji@users.noreply.github.com>
2024-01-17 02:09:33 +0900

    Update clock.js

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -1,11 +1,16 @@
-const clock = document.querySelector("h2");
+const clock = document.querySelector("#clock");
+const today = document.querySelector("#date");
 
 function getClock() {
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}`;
+  today.innerText = `${year}.${month}.${day}.`;
 }
 
 getClock();
