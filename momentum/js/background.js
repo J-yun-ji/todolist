a87a6d69e5f58734518702f54a0efee77728cf75
yunji <66745172+J-yun-ji@users.noreply.github.com>
2024-01-17 02:09:07 +0900

    Update background.js

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,9 +1,10 @@
-const images = ["pinkSky.jpg", "city2.jpg", "sunshine.jpg"];
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.png", "5.jpg", "6.JPG"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `/todolist/momentum/img/${chosenImage}`;
+bgImage.src = `../img/${chosenImage}`;
+bgImage.alt = "background image";
 
 document.body.appendChild(bgImage);
